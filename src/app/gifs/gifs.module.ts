6308769dcc3c10a4daab58cf0ade3cd04bedfa2b
krishee107 <krishee107@gmail.com>
2024-06-03 23:52:12 +0200

    Estructura de la página

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from '../pages/HomePage/home-page.component';
+import { ShearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    ShearchBoxComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }

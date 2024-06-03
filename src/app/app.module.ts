3bd3b55e912317de62fc73ac844cef2e4731231e
krishee107 <krishee107@gmail.com>
2024-06-03 23:05:06 +0200

    Estructura de archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]

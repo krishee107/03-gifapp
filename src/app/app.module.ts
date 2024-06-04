3f5c073cd04250d19f412b05c5ff6a9f85d4550f
krishee107 <krishee107@gmail.com>
2024-06-04 19:13:29 +0200

    llamada Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     GifsModule,
     SharedModule
   ],
--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -1,10 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'NayAtUIKqDLGUaVHCAxbR2KYYv7Rq2Ah';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -25,6 +28,20 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.trim().length === 0) return;
     this.organizeTagsHistory(tag);
+
+    // const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`);
+    // const { data } = await resp.json();
+    // console.log(data);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search?`, { params })
+      .subscribe((resp: any) => { //Observable -> Un observable es una colección de funciones que se ejecutan de forma secuencial o asincrónica.
+        console.log(resp.data);
+      });
 
   }
 }

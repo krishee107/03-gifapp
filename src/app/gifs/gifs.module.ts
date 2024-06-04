e618b6416a1da8a46e72617d493461a48f719fe4
krishee107 <krishee107@gmail.com>
2024-06-04 17:34:06 +0200

    estructura gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from '../pages/HomePage/home-page.component';
 import { ShearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    ShearchBoxComponent
+    ShearchBoxComponent,
+    CardListComponent,
   ],
   imports: [
     CommonModule

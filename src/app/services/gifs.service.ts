7747d7cad186d9dff79eefc1946e96bebdd1524f
krishee107 <krishee107@gmail.com>
2024-06-04 19:45:13 +0200

    Guardar en localstorage el historial

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -26,7 +26,13 @@ export class GifsService {
     this._tagsHistory.unshift(tag);
 
     this._tagsHistory = this._tagsHistory.slice(0, 10);
+    this.saveLocalStorage();
   }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
 
   searchTag(tag: string): void {
     if (tag.trim().length === 0) return;
